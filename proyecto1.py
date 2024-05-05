7e8782075f99cde9633b17f24b9afe50eb21a59f
Jordyr72 <jordy.r.r.8@gmail.com>
2024-05-05 17:46:26 -0500

    Agregando algoritmo Bresenham para revisión

--- proyecto1.py
+++ proyecto1.py
@@ -93,6 +93,83 @@ def algoritmo_DDA():
     
     return list(zip(puntos_x, puntos_y))
 
+def algoritmo_Bresenham():
+    
+    x1 = int(entry_x1.get())
+    y1 = int(entry_y1.get())
+    x2 = int(entry_x2.get())
+    y2 = int(entry_y2.get())
+    
+    #Calcular deltas
+    delta_x = x2 - x1
+    delta_y = y2 - y1
+
+    # Determinar la magnitud de los incrementos
+    if delta_x < 0:
+        inc_x = -1
+    else:
+        inc_x = 1
+    if delta_y < 0:
+        inc_y = -1
+    else:
+        inc_y = 1
+        
+    delta_x = abs(delta_x)
+    delta_y = abs(delta_y)
+
+    # Calcular las constantes
+    if delta_y > delta_x:
+        delta_x, delta_y = delta_y, delta_x
+        interchange = True
+    else:
+        interchange = False
+        
+    #Calcular los deltas * 2 y la p    
+    two_dx = 2 * delta_x
+    two_dy = 2 * delta_y
+    p = two_dy - delta_x
+
+    # Variables de inicio
+    x = x1
+    y = y1
+
+    # Inicializar las listas para almacenar los puntos
+    puntos_x = []
+    puntos_y = []
+    pk = []
+
+    # Generar puntos usando el algoritmo de Bresenham
+    for _ in range(delta_x):
+        puntos_x.append(x)
+        puntos_y.append(y)
+        pk.append(p)
+
+        if p >= 0:
+            if interchange:
+                x += inc_x
+            else:
+                y += inc_y
+            p -= two_dx
+        if p < 0:
+            if interchange:
+                y += inc_y
+            else:
+                x += inc_x
+            p += two_dy
+
+    fig = plt.figure(figsize=(5, 5), dpi=100)
+    plt.plot(puntos_x, puntos_y)
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.grid(True)
+    plt.title('Gráfica Algoritmo Bresenham')
+
+    canvas = FigureCanvasTkAgg(fig, master=BresenhamFrame)
+    canvas.get_tk_widget().grid(row=4, column=0, columnspan=4, pady=20)
+
+    
+    return list(zip(puntos_x, puntos_y))
+
 
 root = tk.Tk()
 
@@ -171,6 +248,38 @@ VentanaBresenham.minsize(530,800)
 VentanaBresenham.resizable(False, False)
 VentanaBresenham.config(background="#DEBFA9")
 VentanaBresenham.withdraw()  
+
+BresenhamFrame = tk.Frame(VentanaBresenham, background="#DEBFA9")
+BresenhamFrame.pack()
+BresenhamFrame.place(relx=0.5, rely=0, anchor="n")
+
+tk.Label(BresenhamFrame, text="Ingrese los datos", font=titulos, background="#DEBFA9").grid(row=0, column=1, columnspan=2, pady=20)
+
+tk.Label(BresenhamFrame, text="x1:", font=textos, background="#DEBFA9").grid(row=1, column=0, pady=5)
+entry_x1 = tk.Entry(BresenhamFrame, font=textos, width=10)
+entry_x1.grid(row=1, column=1, pady=5)
+
+tk.Label(BresenhamFrame, text="y1:", font=textos, background="#DEBFA9").grid(row=1, column=2, pady=5)
+entry_y1 = tk.Entry(BresenhamFrame, font=textos, width=10)
+entry_y1.grid(row=1, column=3, pady=5)
+
+tk.Label(BresenhamFrame, text="x2:", font=textos, background="#DEBFA9").grid(row=2, column=0, pady=5)
+entry_x2 = tk.Entry(BresenhamFrame, font=textos, width=10)
+entry_x2.grid(row=2, column=1, pady=5)
+
+tk.Label(BresenhamFrame, text="y2:", font=textos, background="#DEBFA9").grid(row=2, column=2, pady=5)
+entry_y2 = tk.Entry(BresenhamFrame, font=textos, width=10)
+entry_y2.grid(row=2, column=3, pady=5)
+
+button = tk.Button(BresenhamFrame, text="Generar Línea", command=algoritmo_Bresenham, font=textos, relief="flat", bg="#2E8F62", activebackground="#486148", activeforeground="#FFFFFF", cursor="hand2")
+button.grid(row=3, column=0, columnspan=4, pady=20)
+button.place(relx=0.5, rely=1.0, anchor="s")
+
+
+
+for widget in BresenhamFrame.winfo_children():
+    widget.grid_configure(padx=10, pady=5)
+
 
 
 VentanaCircunferencia = tk.Toplevel(root)
